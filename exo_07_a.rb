8b6206042c123c3a9c20ebb1f0d470e667c997ca
VickyMayLee <67643443+VickyMayLee@users.noreply.github.com>
2020-07-07 23:05:29 +0200

    Add files via upload

---
+++ exo_07_a.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+#puts "gets.chomp permet à l'utilisateur de renseigner l'information demandée"
---
+++ exo_07_c.rb
@@ -0,0 +1,3 @@
+user_name = gets.chomp
+puts user_name
+#la version a n'inclut pas le > et ne reprend pas la réponse de l'utilisateur
